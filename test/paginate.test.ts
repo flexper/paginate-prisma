ab77287190e300260d64208d5ba7eb948179c9bc
mike <mike@flexper.fr>
2022-10-21 17:55:49 +0200

    feat(test): testing return values

--- test/paginate.test.ts
+++ test/paginate.test.ts
@@ -2,6 +2,7 @@ import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
 import { PrismaClient, User } from '@prisma/client';
 
 import { paginate } from '../src/index';
+import { Paginated } from '../src/types/Pagination';
 import { DataBaseUtils } from './utils/DataBase.utils';
 
 const prisma = new PrismaClient();
@@ -10,8 +11,34 @@ describe('paginate-prisma lib', () => {
   beforeAll(async () => await DataBaseUtils.seed(prisma));
   afterAll(async () => await DataBaseUtils.drop(prisma));
 
-  it("Should return a 'data' array of values", async () => {
-    const results = await paginate<PrismaClient, User>(prisma, 'user', {});
+  describe('return attributes', () => {
+    let paginated: Paginated<User>;
+
+    beforeAll(async () => {
+      paginated = await paginate<PrismaClient, User>(prisma, 'user', {});
+      console.log(paginated);
+    });
+
+    it("Should have a 'data' array of values", async () => {
+      expect(paginated.data.length).not.toBe(0);
+    });
+
+    it("Should return a 'pages' array of values", async () => {
+      expect(paginated.pages).not.toBe(0);
+    });
+
+    it("Should return a 'page' array of values", async () => {
+      expect(paginated.page).not.toBe(0);
+    });
+
+    it("Should return a 'limit' array of values", async () => {
+      expect(paginated.limit).not.toBe(0);
+    });
+
+    it("Should return a 'items' array of values", async () => {
+      expect(paginated.items).not.toBe(0);
+    });
+  });
 
     expect(results.data.length).not.toBe(0);
   });
